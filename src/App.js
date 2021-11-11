ecb9fab27e1f3eb7154f4fcf080f52d906003236
Radosław Madecki <radoslawmadecki@macbook-pro-radosaw-1.home>
2021-11-11 06:47:08 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
+      <div className="light-bar" style={{
+        background: 'hsl(195deg, 20%, 86%)',
+        position: 'fixed',
+        bottom: '0',
+        height: '40%',
+        width: '100%'
+      }}/>
       <CharacterEditor />
       <Footer />
     </>
